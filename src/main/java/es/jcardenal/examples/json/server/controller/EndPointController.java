c053420c20a1183c734d9e86168a21f655db5893
jcardenal <jcarde@thoughtworks.com>
2020-07-11 17:29:09 +0200

    Add a condition to generate a 500 status error when needed

--- src/main/java/es/jcardenal/examples/json/server/controller/EndPointController.java
+++ src/main/java/es/jcardenal/examples/json/server/controller/EndPointController.java
@@ -11,12 +11,16 @@ public class EndPointController {
 
     @GetMapping
     public String rootResponse() {
-        return "Hello! Please, use /endpoint or /endpoint?items=`no.` ";
+        return "Hello! Please, use /endpoint or /endpoint?items=`no.`<br />" +
+                "Use `items=0` to receive a 500 status";
     }
 
     @GetMapping("/endpoint")
     public ProductDTO[] endpoint(@RequestParam(name = "items", required = false) Integer items) {
         int size = (items != null) ? items : 1;
+
+        if (size == 0) throw new RuntimeException("Invalid no. of items");
+
         ProductDTO[] result = new ProductDTO[size];
         for(int i=0; i<size; i++) {
             PriceDTO firstPrice = new PriceDTO("GBP", "3.4" + (i % 10));
