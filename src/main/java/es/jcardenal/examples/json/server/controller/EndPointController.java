9d4d811e679b14e2ce60e7307f4beab182a9e3fd
jcardenal <jcarde@thoughtworks.com>
2020-07-11 20:10:55 +0200

    Moving to a stream model to avoid creating big collections in memory before sending the response

--- src/main/java/es/jcardenal/examples/json/server/controller/EndPointController.java
+++ src/main/java/es/jcardenal/examples/json/server/controller/EndPointController.java
@@ -1,10 +1,11 @@
 package es.jcardenal.examples.json.server.controller;
 
-import es.jcardenal.examples.json.server.dto.PriceDTO;
 import es.jcardenal.examples.json.server.dto.ProductDTO;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.stream.Stream;
 
 @RestController
 public class EndPointController {
@@ -16,24 +17,14 @@ public class EndPointController {
     }
 
     @GetMapping("/endpoint")
-    public ProductDTO[] endpoint(@RequestParam(name = "items", required = false) Integer items) {
+    public Stream<ProductDTO> endpoint(@RequestParam(name = "items", required = false) Integer items) {
         int size = (items != null) ? items : 1;
 
         if (size == 0) throw new RuntimeException("Invalid no. of items");
 
-        ProductDTO[] result = new ProductDTO[size];
-        for(int i=0; i<size; i++) {
-            PriceDTO firstPrice = new PriceDTO("GBP", "3.4" + (i % 10));
-            PriceDTO secondPrice = new PriceDTO( "EUR",  "2.7" + (i % 10));
-            result[i] = new ProductDTO("094871989999484" + i,
-                    "The Product " + (i + 1),
-                    "This is a test product no. " + (i + 1),
-                    "This is an alternative description for product no. " + (i + 1),
-                    "IDXXX"+i,
-                    firstPrice,
-                    secondPrice
-                    );
-        }
-        return result;
+        ProductDTO.reset();
+        Stream<ProductDTO> stream = Stream.generate(ProductDTO::getInstance);
+        return stream.limit(size);
+
     }
 }
--- src/main/java/es/jcardenal/examples/json/server/dto/ProductDTO.java
+++ src/main/java/es/jcardenal/examples/json/server/dto/ProductDTO.java
@@ -13,4 +13,25 @@ public class ProductDTO {
     private String internalID;
     private PriceDTO firstPrice;
     private PriceDTO secondPrice;
+
+    static int count = 0;
+
+    static public ProductDTO getInstance() {
+            PriceDTO firstPrice = new PriceDTO("GBP", "3.4" + (count % 10));
+            PriceDTO secondPrice = new PriceDTO( "EUR",  "2.7" + (count % 10));
+            ProductDTO result = new ProductDTO("094871989999484" + count,
+                    "The Product " + (count + 1),
+                    "This is a test product no. " + (count + 1),
+                    "This is an alternative description for product no. " + (count + 1),
+                    "IDXXX"+count,
+                    firstPrice,
+                    secondPrice
+                    );
+        count++;
+        return result;
+    }
+
+    static public void reset() {
+        count =0;
+    }
 }
