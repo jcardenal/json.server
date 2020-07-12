8e504c9c8f50e19eae2bf3c818f7bb5d960b903d
jcardenal <jcarde@thoughtworks.com>
2020-07-12 12:22:38 +0200

    Fix stream model to be local to the request

--- src/main/java/es/jcardenal/examples/json/server/controller/EndPointController.java
+++ src/main/java/es/jcardenal/examples/json/server/controller/EndPointController.java
@@ -1,5 +1,6 @@
 package es.jcardenal.examples.json.server.controller;
 
+import es.jcardenal.examples.json.server.dto.PriceDTO;
 import es.jcardenal.examples.json.server.dto.ProductDTO;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,9 +23,20 @@ public class EndPointController {
 
         if (size == 0) throw new RuntimeException("Invalid no. of items");
 
-        ProductDTO.reset();
-        Stream<ProductDTO> stream = Stream.generate(ProductDTO::getInstance);
-        return stream.limit(size);
-
+        return Stream.iterate(0, i -> i + 1).limit(size).map(this::getProduct);
     }
+    
+    private ProductDTO getProduct(int i) {
+        PriceDTO firstPrice = new PriceDTO("GBP", "3.4" + (i % 10));
+        PriceDTO secondPrice = new PriceDTO( "EUR",  "2.7" + (i % 10));
+        return new ProductDTO("094871989999484" + i,
+                "The Product " + (i + 1),
+                "This is a test product no. " + (i + 1),
+                "This is an alternative description for product no. " + (i + 1),
+                "IDXXX"+i,
+                firstPrice,
+                secondPrice
+        );
+    }
+    
 }
